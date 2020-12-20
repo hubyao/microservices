2740c8662fe4c3c54759957aec928a0da19d1381
jiangyao <jiangyao626@foxmail.com>
2020-12-20 10:08:03 +0000

    init

---
+++ micro/go.mod
@@ -0,0 +1,11 @@
+module gateway
+
+go 1.13
+
+require (
+	github.com/micro/go-plugins/micro/cors v0.0.0-20200119172437-4fe21aa238fd // indirect
+	github.com/micro/go-plugins/micro/cors/v2 v2.9.1 // indirect
+	github.com/micro/go-plugins/registry/consul v0.0.0-20200119172437-4fe21aa238fd // indirect
+	github.com/micro/go-plugins/registry/consul/v2 v2.9.1 // indirect
+	github.com/micro/micro/v2 v2.9.3 // indirect
+)
--- micro/main.go
+++ micro/main.go
@@ -1,81 +1,34 @@
 package main
 
 import (
-	"log"
-	"net"
-	"net/http"
 
-	"github.com/Allenxuxu/microservices/lib/token"
-	"github.com/Allenxuxu/microservices/lib/tracer"
-	"github.com/Allenxuxu/microservices/lib/wrapper/auth"
-	"github.com/Allenxuxu/microservices/lib/wrapper/breaker/hystrix"
-	"github.com/Allenxuxu/microservices/lib/wrapper/metrics/prometheus"
-	"github.com/Allenxuxu/microservices/lib/wrapper/tracer/opentracing/stdhttp"
-
-	ph "github.com/afex/hystrix-go/hystrix"
-	"github.com/micro/cli"
-	"github.com/micro/go-plugins/micro/cors"
-	"github.com/micro/micro/cmd"
-	"github.com/micro/micro/plugin"
-	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/micro/micro/v2/cmd"
+	_ "github.com/micro/go-plugins/registry/consul/v2"
 )
-
-func init() {
-	token := &token.Token{}
-
-	plugin.Register(cors.NewPlugin())
-
-	plugin.Register(plugin.NewPlugin(
-		plugin.WithName("auth"),
-		plugin.WithHandler(
-			auth.JWTAuthWrapper(token),
-		),
-		plugin.WithFlag(cli.StringFlag{
-			Name:   "consul_address",
-			Usage:  "consul address for K/V",
-			EnvVar: "CONSUL_ADDRESS",
-			Value:  "127.0.0.1:8500",
-		}),
-		plugin.WithInit(func(ctx *cli.Context) error {
-			log.Println(ctx.String("consul_address"))
-			token.InitConfig(ctx.String("consul_address"), "micro", "config", "jwt-key", "key")
-			return nil
-		}),
-	))
-	plugin.Register(plugin.NewPlugin(
-		plugin.WithName("tracer"),
-		plugin.WithHandler(
-			stdhttp.TracerWrapper,
-		),
-	))
-	plugin.Register(plugin.NewPlugin(
-		plugin.WithName("breaker"),
-		plugin.WithHandler(
-			hystrix.BreakerWrapper,
-		),
-	))
-	plugin.Register(plugin.NewPlugin(
-		plugin.WithName("metrics"),
-		plugin.WithHandler(
-			prometheus.MetricsWrapper,
-		),
-	))
-}
 
 const name = "API gateway"
 
+// 在此注册使用的插件
 func main() {
-	stdhttp.SetSamplingFrequency(50)
-	t, io, err := tracer.NewTracer(name, "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer io.Close()
-	opentracing.SetGlobalTracer(t)
+	// 注册插件
+//	plugin.Register(cors.NewPlugin())
 
-	hystrixStreamHandler := ph.NewStreamHandler()
-	hystrixStreamHandler.Start()
-	go http.ListenAndServe(net.JoinHostPort("", "81"), hystrixStreamHandler)
+
+	// plugin.Register(plugin.NewPlugin(
+	// 	plugin.WithHandler(xlog()),
+	// ))
+
 
 	cmd.Init()
 }
+
+
+
+// // 日志处理
+// func xlog() plugin.Handler {
+// 	return func(h http.Handler) http.Handler {
+// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+// 		})
+// 	}
+// }
